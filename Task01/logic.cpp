a746d6b5d6177b88223a1505e08e77490808d590
WAsD-Ddos <mitja3375@gmail.com>
2025-03-19 17:33:18 +0300

    TASK01-ideal

--- Task01/logic.cpp
+++ Task01/logic.cpp
@@ -10,7 +10,15 @@
 double calculate_arithmetical_mean_of_nonzero_elements(int** matrix, int n, int m) {
     double sum_non_zero = 0;
     int count_non_zero = 0;
+    //full-proof
+    if (n <= 0 || m <= 0) {return 0;}
+    if (matrix == nullptr) { return 0;}
+    //test10
+    int  counts = 0;
+    for (int i = 0; i < n; i++){for (int j = 0; j < m; j++){if ((matrix[i][j] == 0)) { counts++; }}}
+    if (counts == (n * m)) {return 0;}
 
+    //logic
     for (int i = 0; i < n; ++i) {
         for (int j = 0; j < m; ++j) {
             if (matrix[i][j] != 0) {
@@ -21,6 +29,7 @@ double calculate_arithmetical_mean_of_nonzero_elements(int** matrix, int n, int 
     }
 
 
-    return sum_non_zero / (n * m);
+    double res = sum_non_zero / count_non_zero;
+    return res;
 
 }
